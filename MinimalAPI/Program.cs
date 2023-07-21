b9109f65811afa4d6b87ae57e354f482f6e34909
Гагнидзе Давид Владимирович <Gagn_ob80@itstep.edu.az>
2023-07-21 16:01:46 +0400

    21.07.2023

---
+++ MinimalAPI/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+
+app.MapGet("/RandPhoneNumb", () =>
+{
+    return "+994 50 123 123";
+});
+
+app.MapPost("/PhoneNumb", async (string start) =>
+{
+    return $"{start} 123 123";
+});
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
